a9db76e85c7b0354d2a732e37e3c89f74f079ac0
Jagureo <jagnzet@gmail.com>
2017-02-13 11:38:47 +0800

    Added Grid map prototype

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DebugScript : MonoBehaviour
+{
+
+    //public Text dbgtxt;
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        //for (int i = 0; i < 10; i++)
+        //    Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_ANDROID
+            Touch myTouch = Input.GetTouch(0);
+            newpos = new Vector3(myTouch.position.x, myTouch.position.y, 1);
+#else
+        //newpos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1);
+#endif
+
+
+    }
+
+    public void ClickA()
+    {
+
+    }
+
+    public void ClickB()
+    {
+        //audio1.PlayOneShot(impact, 0.7F);
+        //dbgtxt.text = "B Clicked";
+    }
+}
---
+++ Assets/Scripts/LevelGenerate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerate : MonoBehaviour {
+
+    public GameObject prefab;
+    public int[,] mapgrid;
+    private int xsize;
+    private int ysize;
+
+    // Use this for initialization
+    void Start () {
+        //xsize = Random.Range(5, 10);
+        //ysize = Random.Range(5, 10);
+        xsize = 6;
+        ysize = 6;
+        mapgrid = new int[xsize, ysize];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(xsize % 2 == 0)
+        {
+            for (int j = 0; j < ysize; j++)
+            {
+                for (int i = 0; i < xsize; i++)
+                {
+                    Instantiate(prefab, new Vector3(i * 1.0f - ((xsize / 2)) * 1.0f - 0.5f, j * 1.0f, 0), Quaternion.identity);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < xsize; i++)
+            {
+                Instantiate(prefab, new Vector3(i * 1.0f - ((xsize / 2) + 1) * 1.0f, 0, 0), Quaternion.identity);
+            }
+        }
+    }
+}
