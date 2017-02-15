aaf3dbe0eb1eed14211cbdd22361c9af7c934f5c
TemplarDon <donovandenzel@gmail.com>
2017-02-15 17:27:44 +0800

    Idle anim

--- Assets/Scripts/turnManage.cs
+++ Assets/Scripts/turnManage.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 using System.Collections;
 
 public class turnManage : MonoBehaviour {
 
     public int turnNumber;
-    //public Text turnNum;
+    public Text turnNum;
 
 	// Use this for initialization
 	void Start () {
-        //turnNum.text = 1.ToString();
+        turnNum.text = 1.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-       //turnNum.text = turnNumber.ToString();
+       turnNum.text = turnNumber.ToString();
     }
 }
