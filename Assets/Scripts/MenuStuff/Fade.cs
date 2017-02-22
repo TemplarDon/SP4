ebc3ff99279fdf151972c6f3bc5c3e96f25b93e6
Outphase <Outphrase@hotmail.sg>
2017-02-22 17:53:15 +0800

    adding character select (in progress)

--- Assets/Scripts/MenuStuff/Fade.cs
+++ Assets/Scripts/MenuStuff/Fade.cs
@@ -42,7 +42,7 @@ public class Fade : MonoBehaviour
         i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
         while (i.color.a < 1.0f)
         {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime));
             yield return null;
         }
         if (i.color.a >= 1.0f)
@@ -57,7 +57,7 @@ public class Fade : MonoBehaviour
         i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
         while (i.color.a > 0.0f)
         {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime));
             yield return null;
         }
     }
