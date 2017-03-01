d7ded8db267a071baf84ec93ca8e932029b433b8
TemplarDon <donovandenzel@gmail.com>
2017-03-01 15:17:02 +0800

    Jotaro animation bug fixed

---
+++ Assets/Scripts/Skills/KoichiSkill.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class KoichiSkill : BaseSkills
+{
+    // Restrict an enemy movement
+    public int Range = 2;
+
+    public override void DoEffect(BaseCharacter user)
+    {
+        LevelGenerate Map = GameObject.Find("MapGeneration").GetComponent<LevelGenerate>();
+
+        Vector3 mapPos = new Vector3(user.pos.x, user.pos.y, 0);
+
+        List<BaseCharacter> AffectedCharacters = Map.GetCharactersInRange(mapPos, Range);
+
+        foreach (BaseCharacter aCharacter in AffectedCharacters)
+        {
+            Modifier toAdd = new Modifier();
+            toAdd.Init(Modifier.MODIFY_TYPE.HEALTH, 2, 1);
+            GameObject.Find(aCharacter.name).GetComponent<BaseCharacter>().AddModifier(toAdd);
+        }
+    }
+}
