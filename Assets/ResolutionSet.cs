df4938b12982cc423813da07bebcd46dad94e158
Jagureo <jagnzet@gmail.com>
2017-03-01 11:43:23 +0800

    fixed resolution

---
+++ Assets/ResolutionSet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResolutionSet : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        //Screen.SetResolution(2560, 1600, false);
+        //Screen.SetResolution(1680, 1050, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
