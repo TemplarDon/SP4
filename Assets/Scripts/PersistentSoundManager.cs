ad94b8b3dfac8d7dff6f434226bc44fe2c4692b4
TemplarDon <donovandenzel@gmail.com>
2017-03-03 03:17:36 +0800

    Changes to sound Bugfix

--- Assets/Scripts/PersistentSoundManager.cs
+++ Assets/Scripts/PersistentSoundManager.cs
@@ -9,7 +9,7 @@ public class PersistentSoundManager : MonoBehaviour {
     public static PersistentSoundManager m_Instance;
 
     bool b_InitialLoad = false;
-    public List<AudioClip> InitList = new List<AudioClip>();
+    //public List<AudioClip> InitList = new List<AudioClip>();
     public Dictionary<string, AudioClip> AudioList = new Dictionary<string, AudioClip>();
 
     string CurrentScene;
@@ -34,68 +34,82 @@ public class PersistentSoundManager : MonoBehaviour {
     {
         if (!b_InitialLoad)
         {
-            foreach (AudioClip aClip in InitList)
-            {
-                switch (aClip.name)
-                {
-                    case "battletheme1":
-                        AudioList.Add("BattleTheme_1", aClip);
-                        break;
+            //foreach (AudioClip aClip in InitList)
+            //{
+            //    switch (aClip.name)
+            //    {
+            //        case "battletheme1":
+            //            AudioList.Add("BattleTheme_1", aClip);
+            //            break;
 
-                    case "battletheme2":
-                        AudioList.Add("BattleTheme_2", aClip);
-                        break;
+            //        case "battletheme2":
+            //            AudioList.Add("BattleTheme_2", aClip);
+            //            break;
 
-                    case "Free Battle":
-                        AudioList.Add("BattlePrepBGM", aClip);
-                        break;
+            //        case "Free Battle":
+            //            AudioList.Add("BattlePrepBGM", aClip);
+            //            break;
 
-                    case "gacha":
-                        AudioList.Add("GachaBGM", aClip);
-                        break;
+            //        case "gacha":
+            //            AudioList.Add("GachaBGM", aClip);
+            //            break;
 
-                    case "Mode Select":
-                        AudioList.Add("ModeSelectBGM", aClip);
-                        break;
+            //        case "Mode Select":
+            //            AudioList.Add("ModeSelectBGM", aClip);
+            //            break;
 
-                    case "Shop":
-                        AudioList.Add("ShopBGM", aClip);
-                        break;
+            //        case "Shop":
+            //            AudioList.Add("ShopBGM", aClip);
+            //            break;
 
-                    case "Story":
-                        AudioList.Add("StoryBGM", aClip);
-                        break;
+            //        case "Story":
+            //            AudioList.Add("StoryBGM", aClip);
+            //            break;
 
-                    case "Title":
-                        AudioList.Add("TitleBGM", aClip);
-                        break;
+            //        case "Title":
+            //            AudioList.Add("TitleBGM", aClip);
+            //            break;
 
-                    case "melee_attack":
-                        AudioList.Add("MeleeAttack", aClip);
-                        break;
+            //        case "melee_attack":
+            //            AudioList.Add("MeleeAttack", aClip);
+            //            break;
 
-                    case "fugo_blast":
-                        AudioList.Add("FugoSkill", aClip);
-                        break;
+            //        case "fugo_blast":
+            //            AudioList.Add("FugoSkill", aClip);
+            //            break;
 
-                    case "Defend":
-                        AudioList.Add("Defend", aClip);
-                        break;
+            //        case "Defend":
+            //            AudioList.Add("Defend", aClip);
+            //            break;
 
-                    case "Heal":
-                        AudioList.Add("Heal", aClip);
-                        break;
+            //        case "Heal":
+            //            AudioList.Add("Heal", aClip);
+            //            break;
 
-                    case "PowerUp":
-                        AudioList.Add("PowerUp", aClip);
-                        break;
+            //        case "PowerUp":
+            //            AudioList.Add("PowerUp", aClip);
+            //            break;
 
-                    case "Walk":
-                        AudioList.Add("Walk", aClip);
-                        break;
-                }
-            }
+            //        case "Walk":
+            //            AudioList.Add("Walk", aClip);
+            //            break;
+            //    }
+            //}
 
+            AudioList.Add("BattleTheme_1", Resources.Load("battletheme1", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("BattleTheme_2", Resources.Load("battletheme2", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("BattlePrepBGM", Resources.Load("Free Battle", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("GachaBGM", Resources.Load("gacha", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("ModeSelectBGM", Resources.Load("Mode Select", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("ShopBGM", Resources.Load("Shop", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("StoryBGM", Resources.Load("Story", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("TitleBGM", Resources.Load("Title", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("MeleeAttack", Resources.Load("melee_attack", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("FugoSkill", Resources.Load("fugo_blast", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("Defend", Resources.Load("Defend", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("Heal", Resources.Load("Heal", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("PowerUp", Resources.Load("PowerUp", typeof(AudioClip)) as AudioClip);
+            AudioList.Add("Walk", Resources.Load("Walk", typeof(AudioClip)) as AudioClip);
             b_InitialLoad = true;
         }
 
